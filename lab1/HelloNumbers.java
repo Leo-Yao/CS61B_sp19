ad013e2f8522e9e3bbccc5d2c438cf34102cf2fd
Leo_Yao24 <872769368@qq.com>
2022-07-28 09:44:38 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int sum = 0 ;
+        for (int i = 0; i < 10; i++) {
+            System.out.print(sum + " ");
+            sum += i ;
+        }
+    }
+}
